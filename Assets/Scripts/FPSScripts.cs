e8f121b3aafae1fae4403aee96bc0657d484bdcb
alexandersimmons0 <simmoa14@my.erau.edu>
2023-10-06 22:45:08 -0700

    Initial commit

---
+++ Assets/Scripts/FPSScripts.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSScripts : MonoBehaviour{
+    public Text fpsText;
+	public float deltaTime;
+
+	void Update () {
+		deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+		float fps = 1.0f / deltaTime;
+		fpsText.text = "FPS: " + Mathf.Ceil (fps).ToString ();
+	}
+}
